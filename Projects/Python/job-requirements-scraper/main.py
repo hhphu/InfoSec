866dfb52ffba3cfb37b63bfa70c65151d46f175b
hhphu <harry.hphu@gmail.com>
2024-05-24 12:58:53 -0700

    update description.txt

--- Projects/Python/job-requirements-scraper/main.py
+++ Projects/Python/job-requirements-scraper/main.py
@@ -1,8 +1,8 @@
-import requests, re, csv
+import requests, re
 from bs4 import BeautifulSoup as bs
 
 PATTERN = r'jobs/view/[^/]+-(\d+)'
-URL = "https://www.linkedin.com/jobs/search?keywords=security%20engineer&location=United%20States&geoId=103644278&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0"
+URL = "https://www.linkedin.com/jobs/search?keywords=Security%20Engineer&location=Washington%2C%20United%20States&geoId=&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0"
 TEMPLATE_JOB_URL = "https://www.linkedin.com/jobs/view/__JOB_ID__"
 
 
@@ -36,15 +36,9 @@ def convert_to_bs_object(content):
 
 
 
-
-
-
-           
-
-
 if __name__ == "__main__":
     url_list = retrieve_jobs_urls(URL)
-    with open("description.csv", 'w') as csv_file:
+    with open("description.txt", 'a') as txt_file:
         for url in url_list:
             '''
                 Sample URL: https://www.linkedin.com/jobs/view/junior-security-engineer-at-aapc-3911563572?position=1&pageNum=0&refId=iJVtEtgGgs%2BOCD7QPRC8zg%3D%3D&trackingId=HqWOFnbH%2B3Uzpske6fMTAg%3D%3D&trk=public_jobs_jserp-result_search-card
@@ -60,4 +54,4 @@ if __name__ == "__main__":
             job_content = convert_to_bs_object(visit_job.content)
             job_description = job_content.find("div", class_="show-more-less-html__markup")
             description = job_description.get_text(separator="\n")
-            csv_file.write(description)
+            txt_file.write(description)
